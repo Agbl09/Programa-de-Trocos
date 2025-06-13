43e03b0d48b727e8ce5827b2d32e312fc83a1b7b
Gabriela Rodrigues <gabriela.souza.investimentos@gmail.com>
2025-06-13 11:58:41 -0300

    Ajustando quebra de linhas

--- mercadoria.py
+++ mercadoria.py
@@ -2,17 +2,17 @@ valor_mercadoria = float(input("Valor da mercadoria: "))
 valor_pago = float(input("Valor pago: "))
 
 if valor_pago == valor_mercadoria:
-    print("Não existe troco!")
+    print("\nNão existe troco!")
 
 elif valor_pago < valor_mercadoria:
     valor_faltante = round(valor_mercadoria - valor_pago, 2)
 
-    print(f"Faltam R${valor_faltante}")
+    print(f"\nFaltam R${valor_faltante}")
 
 elif valor_pago > valor_mercadoria:
     troco = round(valor_pago - valor_mercadoria, 2)
 
-    print(f"Troco: R${troco}")
+    print(f"\nTroco: R${troco}")
 
     moedas_notas = [5, 2, 1, 0.50, 0.25, 0.10, 0.05, 0.01]
 
